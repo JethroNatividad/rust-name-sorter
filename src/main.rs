af07e708f59665cf787e24cd55f35120c429f389
JethroNatividad <natividad.jet@gmail.com>
2024-02-23 17:22:38 +0800

    expand problem

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 // Program that has a list of names, sorts them, and writes them to a file
+// Input: names
+// Process: sort
+// Output: in file separated by newlines
 
 fn main() {
     println!("Hello, world!");
